fae9b59a143deaaf5aeff84ec9a729456bac4fcf
Mukul Howale <howalemukul@gmail.com>
2022-09-28 18:07:20 +0530

    Added basic stylings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from "./components/Navbar.js";
+import TitleImage from "./components/TitleImage.js";
+import Description from "./components/Description.js";
+import Experiences from "./components/Experiences.js";
+import './styles/App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='App'>
+      <Navbar/>
+      <TitleImage/>
+      <Description/>
+      <Experiences/>
+    </div>
   );
 }
 
