7a2b985d83bdaae931b94d56bf8bbd7ec1595330
Mukul Howale <howalemukul@gmail.com>
2022-09-28 19:14:49 +0530

    Created description component and added stylilngs

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "../styles/Description.css";
 
 export default function Description(){
     return(
-        <h1>Description goes here!</h1>
+        <div className="description bg-color common-padding-left">
+            <p className="de-title bg-color">Online Experiences</p>
+            <p className="de-description bg-color">
+                Join unique interactive activities led by 
+                one-of-a-kind hosts—all without leaving home.
+            </p>
+        </div>
     )
 }
