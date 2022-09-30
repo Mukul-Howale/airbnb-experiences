6948ec91a1db2dee6437b9ba5a4d2704f2a205b1
Mukul Howale <howalemukul@gmail.com>
2022-09-30 20:52:29 +0530

    Used props for rendering data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import TitleImage from "./components/TitleImage";
 import Description from "./components/Description";
 import Experiences from "./components/Experiences";
+import swimmer from "./images/swimmer.png";
 import './styles/App.css';
 
 function App() {
@@ -11,7 +12,16 @@ function App() {
       <Navbar/>
       <TitleImage/>
       <Description/>
-      <Experiences/>
+      <div className='cards'>
+        <Experiences
+          thumbnail = {swimmer}
+          rating = {5.0}
+          review = {6}
+          country = "USA"
+          title = "Life lessons with Katie Zaferes"
+          price = {136}
+        />
+      </div>
     </div>
   )
 }
--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,25 +1,28 @@
 import React from "react";
-import swimmer from "../images/swimmer.png";
 import star from "../images/star.png";
 import ellipse from "../images/ellipse.png";
 import "../styles/Experiences.css";
 
-export default function Experiences(){
+export default function Experiences(props){
+
+    const pokemon = ["Bulbasaur", "Charmander" , "Squirtle"]
+    let r = pokemon.map(poke => `<p1>${poke}</p1>`)
+
     return(
         <div className="exp bg-color common-padding-left">
-            <img src={swimmer} alt="Swimmer"></img>
+            <img src={props.thumbnail} alt="Thumbnail"></img>
             <div className="review bg-color">
                 <img src={star} alt="Star" className="star bg-color"></img>
-                <p className="t-1 bg-color font-details">5.0</p>
-                <p className="t-2 bg-color font-details">(6)</p>
+                <p className="t-1 bg-color font-details">{props.rating}</p>
+                <p className="t-2 bg-color font-details">{props.review}</p>
                 <img src={ellipse} alt="Ellipse" className="ellipse bg-color"></img>
-                <p className="t-3 bg-color font-details">USA</p>
+                <p className="t-3 bg-color font-details">{props.country}</p>
             </div>
             <p className="details bg-color font-details">
-                Life lessons with Katie Zaferes
+                {r}
             </p>
             <p className="price bg-color font-details font-weight-600">
-                From $136 <span className="bg-color font-details">/ person</span>
+                From ${props.price} <span className="bg-color font-details">/ person</span>
             </p>
         </div>
     )
